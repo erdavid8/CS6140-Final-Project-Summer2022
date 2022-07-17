20a394b82c5ce7b368ec53c7c77cd0be52ee6d08
Jacqueline Girouard <girouarj@bc.edu>
2022-07-17 13:28:41 -0400

    experiments upload

--- airplane_sat/svm/linear/linear_svm.py
+++ airplane_sat/svm/linear/linear_svm.py
@@ -2,6 +2,8 @@ from sklearn import svm
 import numpy as np 
 import matplotlib.pyplot as plt
 from  airplane_sat.data_prep.data_prep import load_seperate_data
+from airplane_sat.model_eval.eval_stats import get_stats
+import itertools
 
 class FeatureCombo:
     def __init__(self, features, accuracy = 0, precision =0, recall =0, f1=0):
@@ -50,6 +52,9 @@ def svm_linear(x_train, y_train, x_test, y_test, feature_names):
     clf = svm.SVC(kernel='linear')
     clf.fit(x_train, y_train)
 
+    #get stats
+    accuracy,recall, precision, f1 = get_stats(clf, x_test, y_test)
+
     #plot 2D feature
     if len(feature_names) == 2:
         fig, ax = plt.subplots()
@@ -58,21 +63,42 @@ def svm_linear(x_train, y_train, x_test, y_test, feature_names):
         ax.scatter(x_train[:, 0], x_train[:, 1], c=y_train, cmap=plt.cm.coolwarm, s=20, edgecolors='k')
         ax.set_ylabel(feature_names[0])
         ax.set_xlabel(feature_names[1])
-        title = "2D Linar SVM with Features: {} and {}".format(feature_names[0], feature_names[1])
+        title = "2D Linear SVM with Features: {} and {}".format(feature_names[0], feature_names[1])
         ax.set_title(title)
+        plt.suptitle('Accuracy {:.4f}, Recall {:.4f}, Precision {:.4f}, F1 {:.4f}'.format(accuracy,recall, precision, f1), fontsize=8)
         plt.savefig("{}.png".format(title))
-    return FeatureCombo(feature_names, accuracy = 0, precision =0, recall =0, f1=0)
+
+    #plot 3D feature
+    if len(feature_names) == 3:
+        z = lambda x,y: (-clf.intercept_[0]-clf.coef_[0][0]*x -clf.coef_[0][1]*y) / clf.coef_[0][2]
+        tmp = np.linspace(-5,5,30)
+        x,y = np.meshgrid(tmp,tmp)
+
+        fig = plt.figure()
+        ax  = fig.add_subplot(111, projection='3d')
+        ax.plot3D(x_train[y_train==0,0], x_train[y_train==0,1], x_train[y_train==0,2],'ob')
+        ax.plot3D(x_train[y_train==1,0], x_train[y_train==1,1], x_train[y_train==1,2],'sr')
+        ax.plot_surface(x, y, z(x,y))
+        ax.view_init(30, 60)
+        title = "3D Linear SVM with Features: {}, {} and {}".format(feature_names[0], feature_names[1], feature_names[2])
+        ax.set_title(title)
+        plt.suptitle('Accuracy {}, Recall {}, Precision {}, F1 {}'.format(accuracy,recall, precision, f1))
+        plt.savefig("{}.png".format(title))
+
+    
+        
+    return FeatureCombo(feature_names, accuracy, precision, recall, f1)
 
 def svm_linear_2Features():
     ''' 
         Trains and evaluates a linear svm with 2 features
     '''
-    combos_tested = set()
-    combos = []
+    #prep data
     df_train, df_test = load_seperate_data("train.csv", "test.csv")
-    sat = {'satisfied': 1,'neutral or dissatisfied': 0}
+    classes = df_train['satisfaction'].unique()
+    sat = {classes[0]: 1,classes[1]: 0}
     df_train['satisfaction'] = [sat[item] for item in df_train['satisfaction']]
-    df_test['satisfaction'] = [sat[item] for item in df_train['satisfaction']]
+    df_test['satisfaction'] = [sat[item] for item in df_test['satisfaction']]
     y_train = df_train['satisfaction'].to_numpy()
     y_test = df_test['satisfaction'].to_numpy()
 
@@ -81,30 +107,60 @@ def svm_linear_2Features():
     #try all combinations
     category_to_try = ['Seat comfort', 'Online boarding', 'Leg room service', 'Inflight service', \
         'Inflight entertainment', 'Inflight wifi service','Flight Distance', 'Age']
-    for category1 in category_to_try:
-        for category2 in category_to_try:
-            if category1 != category2 and (category1 , category2) not in combos_tested:
-                combos_tested.add((category1 , category2))
-                combos_tested.add((category2 , category1))
-                feature_names = [category1 , category2]
-                x_train = df_train[[category1 , category2]].to_numpy()
-                x_test = df_test[[category1 , category2]].to_numpy()
-                combos.append(svm_linear(x_train, y_train, x_test, y_test, feature_names))
+
+    combos = itertools.combinations(category_to_try, 2)
+    combo_results = []
+    for combo in combos:
+        print("Training combo {} and {}".format(combo[0] ,combo[1]))
+        feature_names = [combo[0] ,combo[1]]
+        x_train = df_train[[combo[0] ,combo[1]]].to_numpy()
+        x_test = df_test[[combo[0] ,combo[1]]].to_numpy()
+        combo_results.append(svm_linear(x_train, y_train, x_test, y_test, feature_names))
 
     #sort all the combos tested by accuracy            
-    combos.sort(key=lambda x: x.accuracy)
+    combo_results.sort(key=lambda x: x.accuracy)
     #print the top ten                
     for i in range(10):
-        combo_features = combos[i].features
+        combo_features = combo_results[i].features
         print("The {}th best combo is {} and {}".format(i, combo_features[0], combo_features[1]))
     
+svm_linear_2Features()
+
 def svm_linear_3Features():
     ''' 
         Trains and evaluates a linear svm with 3 features
     '''
+    #prep data
+    df_train, df_test = load_seperate_data("train.csv", "test.csv")
+    classes = df_train['satisfaction'].unique()
+    sat = {classes[0]: 1,classes[1]: 0}
+    df_train['satisfaction'] = [sat[item] for item in df_train['satisfaction']]
+    df_test['satisfaction'] = [sat[item] for item in df_test['satisfaction']]
+    y_train = df_train['satisfaction'].to_numpy()
+    y_test = df_test['satisfaction'].to_numpy()
+
+
     #based off of initial distrubtions, these categories naively seem most likely to not be noisy
+    #try all combinations
     category_to_try = ['Seat comfort', 'Online boarding', 'Leg room service', 'Inflight service', \
         'Inflight entertainment', 'Inflight wifi service','Flight Distance', 'Age']
+
+    combos = itertools.combinations(category_to_try, 3)
+    combo_results = []
+    for combo in combos:
+        print("Training combo {} , {} and {}".format(combo[0] ,combo[1],combo[2]))
+        feature_names = [combo[0] ,combo[1],combo[2]]
+        x_train = df_train[[combo[0] ,combo[1],combo[2]]].to_numpy()
+        x_test = df_test[[combo[0] ,combo[1],combo[2]]].to_numpy()
+        combo_results.append(svm_linear(x_train, y_train, x_test, y_test, feature_names))
+
+    #sort all the combos tested by accuracy            
+    combo_results.sort(key=lambda x: x.accuracy)
+    #print the top ten                
+    for i in range(10):
+        combo_features = combo_results[i].features
+        print("The {}th best combo is {} , {} and {}".format(i, combo_features[0], combo_features[1], combo_features[2]))
+    
 
 def svm_linear_AllFeatures():
     ''' 
